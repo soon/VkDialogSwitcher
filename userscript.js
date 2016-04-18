68f7986ccf58501268b4a8fb6b182881c4252e92
Andrew <0coming.soon@gmail.com>
2016-04-18 12:34:43 +0500

    Added initial helpers support

--- userscript.js
+++ userscript.js
@@ -1,25 +1,75 @@
 // ==UserScript==
 // @name         VK dialogs switcher
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  Allows to switch between open dialogs using Ctrl + 1, 2 ... shortcut
 // @author       Andrew Kuchev
-// @match        https://new.vk.com/im*
+// @match        https://new.vk.com/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
+    var getDialogNode = function(index) {
+        return document.querySelector('#im_dialogs > li:nth-child(' + index + ') > div');
+    };
+
+    var helperClassName = '_dialog-switcher-helper';
+
+    var appendHelperToDialogNode = function(index) {
+        var dialog = getDialogNode(index);
+        dialog.style.cssText = "position:relative;";
+
+        var helper = document.createElement("DIV");
+        helper.classList.add(helperClassName);
+        helper.textContent = index;
+        helper.style.cssText = (
+            'position: absolute;' +
+            'top: 20px;' +
+            'left: 14px;' +
+            'padding: 2px 3px 2px 3px;' +
+            'background: -webkit-gradient(linear, 0% 0%, 0% 100%, from(rgb(255, 247, 133)), to(rgb(255, 197, 66)));' +
+            'border: 1px solid rgb(227, 190, 35);');
+        dialog.appendChild(helper);
+    };
+
+    var drawHelpers = function() {
+        for (var i = 1; i < 10; ++i) {
+            appendHelperToDialogNode(i);
+        }
+    };
+
+    var removeHelpers = function() {
+        var helpers = document.getElementsByClassName(helperClassName);
+
+        while(helpers[0]) {
+            helpers[0].parentNode.removeChild(helpers[0]);
+        }
+    };
 
     document.onkeydown = function(event) {
         event = event || window.event;
-        if (event.ctrlKey && event.keyCode >= 49 && event.keyCode <= 57) {
-            var index = event.keyCode - 48;
-            var dialog = document.querySelector('#im_dialogs > li:nth-child(' + index + ') > div');
-            dialog.click();
-            document.getElementById('im_dialogs').scrollTop += dialog.getBoundingClientRect().top - 212; // 212 px should be enough for everyone
-            return false;
+        if (event.ctrlKey) {
+            drawHelpers();
+            if (event.keyCode >= 49 && event.keyCode <= 57) {
+                var index = event.keyCode - 48;
+                var dialog = document.querySelector('#im_dialogs > li:nth-child(' + index + ') > div');
+                dialog.click();
+                document.getElementById('im_dialogs').scrollTop += dialog.getBoundingClientRect().top - 212;
+                removeHelpers();
+                return false;
+            }
         }
+
+        return true;
+    };
+
+    document.onkeyup = function(event) {
+        event = event || window.event;
+        if (!event.ctrlKey) {
+            removeHelpers();
+        }
+
         return true;
     };
 })();
