72eb3e33c1f3881d320f432d6a9f0406668e6bac
Andrew <0coming.soon@gmail.com>
2016-04-18 13:47:54 +0500

    Switch tabs when helpers are shown (even if Ctrl is released)
    Ctr+Tab switches between tabs and the script cannot handle `Ctrl` release.

--- userscript.js
+++ userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VK dialogs switcher
 // @namespace    http://tampermonkey.net/
-// @version      0.1.3
+// @version      0.1.4
 // @description  Allows to switch between open dialogs using Ctrl + 1, 2 ... shortcut
 // @author       Andrew Kuchev
 // @match        https://new.vk.com/*
@@ -40,18 +40,29 @@
         }
     };
 
+    var getHelpers = function() {
+        return document.getElementsByClassName(helperClassName);
+    };
+
     var removeHelpers = function() {
-        var helpers = document.getElementsByClassName(helperClassName);
+        var helpers = getHelpers();
 
         while(helpers[0]) {
             helpers[0].parentNode.removeChild(helpers[0]);
         }
     };
 
+    var hasHelpers = function() {
+        return getHelpers().length > 0;
+    };
+
     document.onkeydown = function(event) {
         event = event || window.event;
-        if (event.ctrlKey) {
+        if (event.ctrlKey && !hasHelpers()) {
             drawHelpers();
+        }
+
+        if (hasHelpers()) {
             if (event.keyCode >= 49 && event.keyCode <= 57) {
                 var index = event.keyCode - 48;
                 var dialog = getDialogNode(index);
